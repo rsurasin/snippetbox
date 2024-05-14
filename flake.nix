e8cc4a8f7bae42aeb48f889f41542746a4fa7e58
Rahul Surasinghe <rsurasin@gmail.com>
2024-05-14 10:17:53 -0400

    refactor: Remove gomod2nix in favor of improved flake.nix.

--- flake.nix
+++ flake.nix
@@ -1,29 +1,30 @@
 {
-  description = "A basic gomod2nix flake";
+  description = "A Go 1.22 development environment";
 
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-  inputs.flake-utils.url = "github:numtide/flake-utils";
-  inputs.gomod2nix.url = "github:nix-community/gomod2nix";
-  inputs.gomod2nix.inputs.nixpkgs.follows = "nixpkgs";
-  inputs.gomod2nix.inputs.flake-utils.follows = "flake-utils";
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
 
-  outputs = { self, nixpkgs, flake-utils, gomod2nix }:
-    (flake-utils.lib.eachDefaultSystem
-      (system:
-        let
-          pkgs = nixpkgs.legacyPackages.${system};
+  # Use flake-utils to simplify flake setup across multiple systems
+  outputs = { self, nixpkgs, flake-utils, ... }@inputs:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        # Import the nixpkgs with overlay and configuration
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [];
+        };
+        in {
+          # Define a development shell
+          devShell = pkgs.mkShell {
+            buildInputs = [
+              pkgs.go_1_22
+              pkgs.gopls
+            ];
 
-          # The current default sdk for macOS fails to compile go projects, so we use a newer one for now.
-          # This has no effect on other platforms.
-          callPackage = pkgs.darwin.apple_sdk_11_0.callPackage or pkgs.callPackage;
-        in
-        {
-          packages.default = callPackage ./. {
-            inherit (gomod2nix.legacyPackages.${system}) buildGoApplication;
+            # Optional: Set GOPATH and other env variables
           };
-          devShells.default = callPackage ./shell.nix {
-            inherit (gomod2nix.legacyPackages.${system}) mkGoEnv gomod2nix;
-          };
-        })
+        }
     );
 }
