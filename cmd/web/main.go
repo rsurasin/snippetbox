c370c96829405ca74e78636e0b772445f6b73e12
Rahul Surasinghe <rsurasin@gmail.com>
2024-05-15 16:35:59 -0400

    feat: Utilize dependency injection w/ app struct that contains structured logger.

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -7,6 +7,10 @@ import (
 	"os"
 )
 
+type application struct {
+	logger *slog.Logger
+}
+
 func main() {
 	// Define cmd line arg: addr w/ default value :4000
 	addr := flag.String("addr", ":4000", "HTTP Network Connection")
@@ -15,14 +19,15 @@ func main() {
 
 	// Create structured logger - uses default settings
 	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
+	app := &application{logger: logger}
 
 	mux := http.NewServeMux()
 	fileserver := http.FileServer(http.Dir("./ui/static/"))
 	mux.Handle("GET /static/", http.StripPrefix("/static", fileserver))
-	mux.HandleFunc("GET /{$}", home)
-	mux.HandleFunc("GET /snippet/view/{id}", snippetView)
-	mux.HandleFunc("GET /snippet/create", snippetCreate)
-	mux.HandleFunc("POST /snippet/create", snippetCreatePost)
+	mux.HandleFunc("GET /{$}", app.home)
+	mux.HandleFunc("GET /snippet/view/{id}", app.snippetView)
+	mux.HandleFunc("GET /snippet/create", app.snippetCreate)
+	mux.HandleFunc("POST /snippet/create", app.snippetCreatePost)
 
 	logger.Info("Starting Server", "addr", *addr)
 
