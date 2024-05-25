42a65c8570589f4b6b0694371cf8f5048d30fc41
Rahul Surasinghe <rsurasin@gmail.com>
2024-05-24 22:28:58 -0400

    feat: Add MySQL/MariaDB support.

--- flake.nix
+++ flake.nix
@@ -21,9 +21,33 @@
             buildInputs = [
               pkgs.go_1_22
               pkgs.gopls
+              pkgs.mysql
             ];
 
             # Optional: Set GOPATH and other env variables
+            shellHook = ''
+              # Directory for MySQL data
+              if [ ! -d "$PWD/mysql-data" ]; then
+                echo "Initializing MySQL data directory..."
+                mysql_install_db --datadir=$PWD/mysql-data --auth-root-authentication-method=normal
+              fi
+
+              # Check if MySQL Server is already running
+              if ! mysqladmin ping --socket=$PWD/mysql.sock --silent; then
+                echo "Starting MySQL server..."
+                mysqld --datadir=$PWD/mysql-data --socket=$PWD/mysql.sock --pid-file=$PWD/mysql.pid &
+                export MYSQL_UNIX_PORT=$PWD/mysql.sock
+
+                echo "Waiting for MySQL server to start..."
+                while ! mysqladmin ping --socket=$MYSQL_UNIX_PORT --silent; do
+                  sleep 1
+                done
+                echo "MySQL server started."
+              else
+                echo "MySQL server is already running."
+                export MYSQL_UNIX_PORT=$PWD/mysql.sock
+              fi
+            '';
           };
         }
     );
