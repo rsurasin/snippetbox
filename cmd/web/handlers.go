9cbc23ab68cefd808486648bf76fdb9279b5d608
Rahul Surasinghe <rsurasin@gmail.com>
2024-06-09 12:59:40 -0400

    feat: Add common dynamic data (CurrentYear)

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -18,9 +18,8 @@ func (app *application) home(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	data := templateData{
-		Snippets: snippets,
-	}
+	data := app.newTemplateData(r)
+	data.Snippets = snippets
 
 	app.render(w, r, http.StatusOK, "home.tmpl", data)
 }
@@ -42,7 +41,8 @@ func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	data := templateData{Snippet: snippet}
+	data := app.newTemplateData(r)
+	data.Snippet = snippet
 
 	app.render(w, r, http.StatusOK, "view.tmpl", data)
 }
